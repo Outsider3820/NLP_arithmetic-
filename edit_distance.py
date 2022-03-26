6c6f69b798f3458b077388424ec749edd8e54db7
Outsider <205733801@qq.com>
2022-03-26 21:56:59 +0800

    update

--- edit_distance.py
+++ edit_distance.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+'''
+编辑距离是指两个字串之间，由一个转成另一个所需的最少编辑操作次数。
+编辑操作包括将一个字符替换成另一个字符，插入一个字符，删除一个字符。
+一般来说，编辑距离越小，两个串的相似度越大。
+'''
+
 def edit_distance(sentence1, sentence2):
     len1 = len(sentence1)
     len2 = len(sentence2)
